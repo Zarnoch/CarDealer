665db3c2007e48bfff0a28ff1694731aabb72360
Zarnoch <arkadiusz.zarnoch@gmail.com>
2018-06-23 12:12:44 +0200

    Xamarin auto changes

--- CarDealer/obj/Debug/designtime/Resource.Designer.cs
+++ CarDealer/obj/Debug/designtime/Resource.Designer.cs
@@ -24,6 +24,10 @@ namespace CarDealer
 			global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 		}
 		
+		public static void UpdateIdValues()
+		{
+		}
+		
 		public partial class Animation
 		{
 			
